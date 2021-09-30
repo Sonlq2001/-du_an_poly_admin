5c8c9b3d2a3bb7674c310a453c8e3ae5f67138ec
Sonlq2001 <sonweb2001@gmail.com>
2021-09-30 22:31:56 +0700

    UI upload excel

--- src/features/product/screens/ProductScreen.jsx
+++ src/features/product/screens/ProductScreen.jsx
@@ -7,249 +7,249 @@ import { Button } from "./../../../components/Button/Button";
 import { TablePagination } from "./../../../components/Pagination/Pagination";
 import ProductControlTable from "./../components/ProductControlTable/ProductControlTable";
 import {
-  TableCustom,
-  Thead,
-  Tr,
-  Th,
-  Tbody,
-  Td,
+	TableCustom,
+	Thead,
+	Tr,
+	Th,
+	Tbody,
+	Td,
 } from "./../../../components/Table/TableCustom";
 const ProductScreen = () => {
-  const [rowPerPage, setRowPerPage] = useState(10);
-  const handleChangeRowsPerPage = (rowPage) => {
-    setRowPerPage(rowPage);
-  };
+	const [rowPerPage, setRowPerPage] = useState(10);
+	const handleChangeRowsPerPage = (rowPage) => {
+		setRowPerPage(rowPage);
+	};
 
-  const handleChangePage = () => {
-    console.log("vo day");
-  };
-  return (
-    <WrapContent>
-      <TitleTable> Danh sách sản phẩm </TitleTable>
-      <ProductControlTable />
-      <TableCustom>
-        <Thead>
-          <Tr>
-            <Th sort={false}> STT </Th>
-            <Th> Tên đề tài </Th>
-            <Th> Môn </Th>
-            <Th> Lớp </Th>
-            <Th> Giảng viên </Th>
-            <Th sort={false}> Nhóm </Th>
-            <Th sort={false}> Action </Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          <Tr>
-            <Td> 1</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> dự án tốt nghiệp </Td>
-            <Td> p115315</Td>
-            <Td> Trần Hữu Thiện</Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td> 2</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> dự án tốt nghiệp </Td>
-            <Td> p115315</Td>
-            <Td> Trần Hữu Thiện</Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td> 2</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> dự án tốt nghiệp </Td>
-            <Td> p115315</Td>
-            <Td> Trần Hữu Thiện</Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td> 3</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> dự án tốt nghiệp </Td>
-            <Td> p115315</Td>
-            <Td> Trần Hữu Thiện</Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td> 4</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> dự án tốt nghiệp </Td>
-            <Td> p115315</Td>
-            <Td> Trần Hữu Thiện</Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td> 3</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> dự án tốt nghiệp </Td>
-            <Td> p115315</Td>
-            <Td> Trần Hữu Thiện</Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td> 4</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> dự án tốt nghiệp </Td>
-            <Td> p115315</Td>
-            <Td> Trần Hữu Thiện</Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td> 5</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> dự án tốt nghiệp </Td>
-            <Td> p115315</Td>
-            <Td>Lê Trọng Đạt </Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td> 6</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> dự án tốt nghiệp </Td>
-            <Td> p115315</Td>
-            <Td> Trần Hữu Thiện</Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td> 7</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> dự án tốt nghiệp </Td>
-            <Td> p115315</Td>
-            <Td> Trần Hữu Thiện</Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-          <Tr>
-            <Td> 8</Td>
-            <Td> Sản phẩm poly </Td>
-            <Td> Dự án 1 </Td>
-            <Td> p115315</Td>
-            <Td> Trần Hữu Thiện</Td>
-            <Td>
-              <p>Lê Quang Sơn , lê duy sơn ...</p>
-            </Td>
-            <Td>
-              <Button color="warning">
-                <MdModeEdit />
-              </Button>
-              <Button color="danger">
-                <BsTrash />
-              </Button>
-            </Td>
-          </Tr>
-        </Tbody>
-      </TableCustom>
-      <GroupPagination>
-        <TablePagination
-          onRowsPerPageChange={handleChangeRowsPerPage}
-          rowsPerPageOptions={[10, 20, 50]}
-          rowsPerPage={rowPerPage}
-          onPageChange={handleChangePage}
-        />
-      </GroupPagination>
-    </WrapContent>
-  );
+	const handleChangePage = () => {
+		console.log("vo day");
+	};
+	return (
+		<WrapContent>
+			<TitleTable> Danh sách sản phẩm </TitleTable>
+			<ProductControlTable />
+			<TableCustom>
+				<Thead>
+					<Tr>
+						<Th sort={false}> STT </Th>
+						<Th> Tên đề tài </Th>
+						<Th> Môn </Th>
+						<Th> Lớp </Th>
+						<Th> Giảng viên </Th>
+						<Th sort={false}> Nhóm </Th>
+						<Th sort={false}> Action </Th>
+					</Tr>
+				</Thead>
+				<Tbody>
+					<Tr>
+						<Td> 1</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> dự án tốt nghiệp </Td>
+						<Td> p115315</Td>
+						<Td> Trần Hữu Thiện</Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+					<Tr>
+						<Td> 2</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> dự án tốt nghiệp </Td>
+						<Td> p115315</Td>
+						<Td> Trần Hữu Thiện</Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+					<Tr>
+						<Td> 2</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> dự án tốt nghiệp </Td>
+						<Td> p115315</Td>
+						<Td> Trần Hữu Thiện</Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+					<Tr>
+						<Td> 3</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> dự án tốt nghiệp </Td>
+						<Td> p115315</Td>
+						<Td> Trần Hữu Thiện</Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+					<Tr>
+						<Td> 4</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> dự án tốt nghiệp </Td>
+						<Td> p115315</Td>
+						<Td> Trần Hữu Thiện</Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+					<Tr>
+						<Td> 3</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> dự án tốt nghiệp </Td>
+						<Td> p115315</Td>
+						<Td> Trần Hữu Thiện</Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+					<Tr>
+						<Td> 4</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> dự án tốt nghiệp </Td>
+						<Td> p115315</Td>
+						<Td> Trần Hữu Thiện</Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+					<Tr>
+						<Td> 5</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> dự án tốt nghiệp </Td>
+						<Td> p115315</Td>
+						<Td>Lê Trọng Đạt </Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+					<Tr>
+						<Td> 6</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> dự án tốt nghiệp </Td>
+						<Td> p115315</Td>
+						<Td> Trần Hữu Thiện</Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+					<Tr>
+						<Td> 7</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> dự án tốt nghiệp </Td>
+						<Td> p115315</Td>
+						<Td> Trần Hữu Thiện</Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+					<Tr>
+						<Td> 8</Td>
+						<Td> Sản phẩm poly </Td>
+						<Td> Dự án 1 </Td>
+						<Td> p115315</Td>
+						<Td> Trần Hữu Thiện</Td>
+						<Td>
+							<p>Lê Quang Sơn , lê duy sơn ...</p>
+						</Td>
+						<Td>
+							<Button color="warning">
+								<MdModeEdit />
+							</Button>
+							<Button color="danger">
+								<BsTrash />
+							</Button>
+						</Td>
+					</Tr>
+				</Tbody>
+			</TableCustom>
+			<GroupPagination>
+				<TablePagination
+					onRowsPerPageChange={handleChangeRowsPerPage}
+					rowsPerPageOptions={[10, 20, 50]}
+					rowsPerPage={rowPerPage}
+					onPageChange={handleChangePage}
+				/>
+			</GroupPagination>
+		</WrapContent>
+	);
 };
 
 export default ProductScreen;
--- src/routes/routes.config.js
+++ src/routes/routes.config.js
@@ -3,10 +3,13 @@ import { CONFIRM_ROUTES } from "./../features/confirm/routes/confirm.routes";
 import { USER_LIST_ROUTES } from "././../features/user/routes/user.routes";
 import { SPCIALIZED_ROUTES } from "../features/specialized/routes/specialized.routes";
 import { PRODUCT_LIST_ROUTES } from "../features/product/routes/product.routes";
+import { UPLOAD_EXCEL_ROUTES } from "./../features/uploadExcel/routes/upload-excel.routes";
+
 export const LIST_ROUTES = [
-  ...SPCIALIZED_ROUTES,
-  ...PRODUCT_LIST_ROUTES,
-  ...USER_LIST_ROUTES,
-  ...CONFIRM_ROUTES,
-  ...DASHBOARD_ROUTES,
+	...UPLOAD_EXCEL_ROUTES,
+	...SPCIALIZED_ROUTES,
+	...PRODUCT_LIST_ROUTES,
+	...USER_LIST_ROUTES,
+	...CONFIRM_ROUTES,
+	...DASHBOARD_ROUTES,
 ];
