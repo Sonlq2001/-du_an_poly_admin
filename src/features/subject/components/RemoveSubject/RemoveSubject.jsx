91c2deb7ec571a0702dff0d4a36fc6dec637ee89
son2210 <z0zb0yz0z@gmail.com>
2021-10-13 00:05:42 +0700

    CRUD subject

--- src/features/subject/components/RemoveSubject/RemoveSubject.jsx
+++ src/features/subject/components/RemoveSubject/RemoveSubject.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { removeSubject } from '../../redux/subject.slice';
 import PopupOverlay from './../../../../components/PopupOverlay/PopupOverlay';
 
 import { MessagePopup } from './RemoveSubject.styles';
-
+import { useDispatch } from 'react-redux';
+import { unwrapResult } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
 const RemoveSubject = ({ item, open, setOpen }) => {
+  const dispatch = useDispatch();
+  const handleRemove = () => {
+    dispatch(removeSubject(item.id))
+      .then(unwrapResult)
+      .then(toast.success('Xóa môn học thành công !'));
+    setOpen(false);
+  };
   return (
-    <PopupOverlay open={open} setOpen={setOpen} isAction textOk="Đồng ý">
+    <PopupOverlay
+      open={open}
+      setOpen={setOpen}
+      onOk={handleRemove}
+      isAction
+      textOk="Đồng ý"
+    >
       <MessagePopup>Bạn có thực sư muốn xóa nội dung này !</MessagePopup>
     </PopupOverlay>
   );
