584e5324ca3570076456f66a1a6f74dbd3099db3
Sonlq2001 <sonweb2001@gmail.com>
2021-11-13 08:59:19 +0700

    redirect after login

--- src/features/auth/routes/auth.routes.js
+++ src/features/auth/routes/auth.routes.js
@@ -1,9 +1,9 @@
 import { lazy } from 'react';
-import { PATH_AUTH } from './../constants/auth.paths';
+import { AUTH_PATHS } from './../constants/auth.paths';
 const AuthLayout = lazy(() => import('../layouts/AuthLayoutScreens'));
 export const SIGN_IN_SCREEN = {
   id: 'id_sign_in',
-  path: PATH_AUTH.SIGN_IN,
+  path: AUTH_PATHS.SIGN_IN,
   component: lazy(() => import('./../screens/SignIn/SignInScreen')),
   layout: AuthLayout,
   isAuthRoute: true,
